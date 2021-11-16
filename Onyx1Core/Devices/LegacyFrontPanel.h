3124392986b900a16f8d0c8a5b0464598dc3839c
John Antypas <ja@antypas.net>
2021-11-15 18:56:02 -0800

    Add a device handler section

---
+++ Onyx1Core/Devices/LegacyFrontPanel.h
@@ -0,0 +1,14 @@
+//
+// Created by jantypas on 11/15/21.
+//
+
+#ifndef ONYX1_LEGACYFRONTPANEL_H
+#define ONYX1_LEGACYFRONTPANEL_H
+
+
+class LegacyFrontPanel {
+
+};
+
+
+#endif //ONYX1_LEGACYFRONTPANEL_H
---
+++ Onyx1Core/Devices/LegacyTape.h
@@ -0,0 +1,14 @@
+//
+// Created by jantypas on 11/15/21.
+//
+
+#ifndef ONYX1_LEGACYTAPE_H
+#define ONYX1_LEGACYTAPE_H
+
+
+class LegacyTape {
+
+};
+
+
+#endif //ONYX1_LEGACYTAPE_H
